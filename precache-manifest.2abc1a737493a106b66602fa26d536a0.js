fa263ce5da2e75c79cd954425f558ace599714e9
Sinch <sinch@xyz.com>
2020-10-01 13:53:26 +0530

    Updates

---
+++ precache-manifest.2abc1a737493a106b66602fa26d536a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa65613c7bb8de34035dae2f78010cba",
+    "url": "/iTask/index.html"
+  },
+  {
+    "revision": "62ac206574cf53480e97",
+    "url": "/iTask/static/css/main.ea97b029.chunk.css"
+  },
+  {
+    "revision": "ca5aa0cd3a5c47ed43cf",
+    "url": "/iTask/static/js/2.807bf6ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iTask/static/js/2.807bf6ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62ac206574cf53480e97",
+    "url": "/iTask/static/js/main.19f9be2f.chunk.js"
+  },
+  {
+    "revision": "bef0790a4d8c0c017ed4",
+    "url": "/iTask/static/js/runtime-main.e9b47252.js"
+  }
+]);
